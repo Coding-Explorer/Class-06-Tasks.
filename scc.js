ce2596082abef1826884b3306deea2c8c687adfe
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:44:59 +0500

    Add files via upload

---
+++ scc.js
@@ -0,0 +1,4 @@
+//Example of Conditional Ternary Operators.
+var isCraving = true;
+var definitelyEat = isCraving ? "chocolate" : "water";
+console.log("you should have to eat some ".concat(definitelyEat, "."));
---
+++ scc.ts
@@ -0,0 +1,7 @@
+//Example of Conditional Ternary Operators.
+ 
+let isCraving = true;
+let definitelyEat = isCraving ? "chocolate" : "water";
+console.log(`you should have to eat some ${definitelyEat}.`);
+
+//Alternatively if the statement will declare false it will print the condition of not craving.

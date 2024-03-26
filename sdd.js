be91c36d0e396706a6e32d42fb58b8ba989b9b78
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:45:37 +0500

    Add files via upload

---
+++ sdd.js
@@ -0,0 +1,12 @@
+//Example of Switch Statements.
+var holiday = "friday";
+switch (holiday) {
+    case "saturday":
+        console.log("Go for shopping");
+        break;
+    case "friday":
+        console.log("Read a good book.");
+        break;
+    default:
+        console.log("Follow a normal routine.");
+}
---
+++ sdd.ts
@@ -0,0 +1,13 @@
+//Example of Switch Statements.
+
+let holiday = "friday";
+switch (holiday) {
+    case "saturday":
+        console.log("Go for shopping");
+        break;
+        case "friday":
+            console.log("Read a good book.");
+            break;
+            default:
+                console.log("Follow a normal routine.");
+}

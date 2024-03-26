e5c12b832a5d519f1e9c5716baee25519d2a9967
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:41:36 +0500

    Add files via upload

---
+++ saa.js
@@ -0,0 +1,8 @@
+//If and If-Else Statements.
+var isGoing = true;
+if (isGoing) {
+    console.log("Wear a red dress.");
+}
+else {
+    console.log("Wear casual dress");
+}
---
+++ saa.ts
@@ -0,0 +1,8 @@
+//If and If-Else Statements.
+
+let isGoing = true;
+if (isGoing) {
+    console.log("Wear a red dress.");
+} else {
+    console.log("Wear casual dress");
+}

061e2b5355ff23ef1f26f943a33bc1128843d71a
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:42:15 +0500

    Add files via upload

---
+++ sbb.js
@@ -0,0 +1,11 @@
+//Example of Else If Statement.
+var isReception = "at night";
+if (isReception === "not at night") {
+    console.log("Wear simple light dress.");
+}
+else if (isReception === "at night") {
+    console.log("Wear a Special Red dress.");
+}
+else {
+    console.log("Wear casual dress.");
+}
---
+++ sbb.ts
@@ -0,0 +1,10 @@
+//Example of Else If Statement.
+
+let isReception = "at night";
+if (isReception === "not at night") {
+    console.log("Wear simple light dress.");
+} else if (isReception === "at night") {
+    console.log("Wear a Special Red dress.");
+} else {
+    console.log("Wear casual dress.");
+}

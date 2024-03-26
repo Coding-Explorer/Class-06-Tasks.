f296fa511fcb4087ef80d0715af4406c3d10fddc
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:43:53 +0500

    This is the BMI Calculator made by Me.

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! user/bin/env node
+
+import inquirer from "inquirer";
+
+const answer1 = await inquirer.prompt([{
+   message: "Enter your weight in kg", type: "number", name: "weight"
+}]);
+
+const answer2 = await inquirer.prompt([{
+    message : "Enter your height in Meters", type : "number", name : "meters"
+}]);
+
+
+
+     let weightInkg = 65  //65kg
+     let heightInmeters = 1.68  //1.68m
+     let bmi = weightInkg /(heightInmeters * heightInmeters);
+     console.log(`Your BMI is ${bmi}`);
